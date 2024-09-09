81c5baaa3c18c384155a19bafee3f719fd86d25c
bernardopire123 <169068301+bernardopire123@users.noreply.github.com>
2024-09-09 10:43:55 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("digitar seu nome e sobrenome:");
+    
+    String nome = sc.nextLine();
+    
+    System.out.println("qual sua idade:");
+    
+    String idade = sc.nextLine();
+      
+    System.out.println("qual seu sexo:");
+    
+    String sexo = sc.nextLine();
+
+    System.out.println("qual sua altura:");
+
+    String altura = sc.nextLine();
+
+    if (sexo.equals("masculino"));
+    
+    System.out.println("olá " + nome + ",fizemos uma breve pesquisa  das informacoes passadas e temos as seguintes informações a repassar");
+    System.out.println("nome: " + nome);
+    System.out.println("idade: " + idade);
+
+    
+
+    
+    
+    
+    
+
+  }
+
+}
